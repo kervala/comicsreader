ab3ce26d6c0a1a6daa4991308d6b281281e67408
Kervala <983521+kervala@users.noreply.github.com>
2013-02-06 12:53:11 +0100

    Added: Forgotten files

---
+++ src/net/kervala/comicsreader/CbtAlbum.java
@@ -0,0 +1,137 @@
+/*
+ * ComicsReader is an Android application to read comics
+ * Copyright (C) 2011-2013 Cedric OCHS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package net.kervala.comicsreader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class CbtAlbum extends Album {
+	private TarFile mTar;
+
+	static final String cbtMimeType = "application/x-cbt";
+	static final String cbtExtension = "cbt";
+	
+	public String getExtension() {
+		return cbtExtension;
+	}
+
+	public String getMimeType() {
+		return cbtMimeType;
+	}
+
+	public static boolean isValid(String filename) {
+		File file = new File(filename);
+
+		boolean valid = false;
+
+		if (file.isFile() && file.canRead() && (file.length() % 512 == 0)) {
+			try {
+				FileInputStream ifs = new FileInputStream(file);
+
+				if (ifs.skip(99) == 99) {
+					byte [] buffer = new byte[1];
+					
+					if (ifs.read(buffer, 0, 1) == 1) {
+						if (buffer[0] == 0) {
+							valid = true;
+						}
+					}
+				}
+
+				ifs.close();
+			} catch (FileNotFoundException e) {
+			} catch (IOException e) {
+			}
+		}
+
+		return valid;
+	}
+
+	public static boolean askConfirm(String filename) {
+		return false;
+	}
+	
+	static String getTitle(String filename) {
+		String title = filename;
+
+		int posExt = title.lastIndexOf(".");
+		if (posExt > -1) title = title.substring(0, posExt);
+
+		int posPath = title.lastIndexOf("/");
+		if (posPath > -1) title = title.substring(posPath + 1);
+
+		return title;
+	}
+	
+	public static String getMimeType(String filename) {
+		return cbtMimeType;
+	}
+	
+	boolean loadFiles() {
+		try {
+			// open RAR file
+			mTar = new TarFile(mFilename);
+		} catch (IOException e) {
+			return false;
+		}
+
+		List<String> files = mTar.entries();
+
+		for(String filename: files) {
+			if (isValidImage(filename)) {
+				mFiles.add(filename);
+			}
+		}
+
+		if (mFiles.isEmpty()) return false;
+		
+		// generate a title from filename
+		int first = mFilename.lastIndexOf("/");
+			
+		if (first != -1) {
+			mTitle = mFilename.substring(first+1, mFilename.length());
+		} else {
+			mTitle = mFilename;
+		}
+
+		int last = mTitle.lastIndexOf(".");
+			
+		if (last != -1) {
+			mTitle = mTitle.substring(0, last);
+		}
+		
+		return true;
+	}
+
+	public void close() {
+		super.close();
+
+		if (mTar != null)
+			mTar.close();
+	}
+
+	protected byte [] getBytes(int page) throws IOException {
+		// get a buffer on a page
+		return mTar.getBytes(mFiles.get(page));
+	}
+}
---
+++ src/net/kervala/comicsreader/TarFile.java
@@ -0,0 +1,214 @@
+/*
+ * ComicsReader is an Android application to read comics
+ * Copyright (C) 2011-2013 Cedric OCHS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package net.kervala.comicsreader;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.util.Log;
+
+public class TarFile {
+	private class TarEntry {
+		String filename;
+		int offset = 0;
+		int filesize = 0;
+	}
+	
+	protected String mName;
+	protected List<TarEntry> mEntries = new ArrayList<TarEntry>();
+
+	public TarFile(File file) throws IOException {
+		if (file == null || !open(file.getAbsolutePath())) {
+			throw new IOException();
+		}
+	}
+
+	public TarFile(String filename) throws IOException {
+		open(filename);
+	}
+
+	private boolean open(String filename) throws IOException {
+		if (filename == null) {
+			throw new IOException();
+		}
+		
+		mName = filename;
+
+		return readEntries();
+	}
+
+	public void close() {
+		mEntries = null;
+		mName = null;
+	}
+
+	public List<String> entries() {
+		List<String> entries = new ArrayList<String>();
+
+		for(TarEntry entry: mEntries) {
+			entries.add(entry.filename);
+		}
+
+		return entries;
+	}
+
+	public byte [] getBytes(String entry) {
+		byte [] buffer = null;
+		
+		for(TarEntry ent: mEntries) {
+			if (ent.filename != null && ent.filename.equals(entry)) {
+				File file = new File(mName);
+
+				try {
+					InputStream is = new FileInputStream(file);
+					BufferedInputStream in = new BufferedInputStream(is);
+
+					if (in.skip(ent.offset) == ent.offset) {
+						buffer = Album.inputStreamToBytes(in, ent.filesize);
+					}
+					
+					is.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			
+				break;
+			}
+		}
+
+		return buffer;
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public int size() {
+		return mEntries.size();
+	}
+	
+	private boolean readEntries() {
+		byte [] bufferFilename = new byte[100];
+		byte [] bufferFilesize = new byte[12];
+		
+		BufferedInputStream in = null;
+		
+		File file = new File(mName);
+		
+		boolean res = false;
+		int offset = 0;
+		
+		try {
+			in = new BufferedInputStream(new FileInputStream(file));
+
+			while(in.read(bufferFilename, 0, bufferFilename.length) == bufferFilename.length) {
+				String filename = new String(bufferFilename, "UTF-8");
+
+				// we are in padding, this is the end
+				if (bufferFilename[0] == 0) {
+					res = true;
+					break;
+				}
+
+				int filesize = 0;
+				
+				// file mode
+				in.skip(8);
+
+				// owner
+				in.skip(8);
+
+				// group
+				in.skip(8);
+
+				if (in.read(bufferFilesize, 0, bufferFilesize.length) == bufferFilesize.length) {
+					String s = new String(bufferFilesize, "UTF-8");
+					
+					try {
+						filesize = Integer.parseInt(s.trim(), 8);
+					} catch(NumberFormatException e) {
+						break;
+					}
+				}
+
+				// date
+				in.skip(12);
+
+				// checksum
+				in.skip(8);
+
+				// file type
+				in.skip(1);
+
+				// linked filename
+				in.skip(100);
+				
+				// misc
+				in.skip(255);
+				
+				// skip header
+				offset += 512;
+				
+				if (filesize > 0) {
+					// skip file content
+					in.skip(filesize);
+					
+					int len = 512 - (filesize % 512);
+					
+					if (len > 0) {
+						in.skip(len);
+					}
+
+					if (filename != null) {
+						TarEntry entry = new TarEntry();
+						entry.filename = filename.trim();
+						entry.filesize = filesize;
+						entry.offset = offset;
+						mEntries.add(entry);
+					}
+					
+					offset += filesize + len;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return res;
+	}
+}
