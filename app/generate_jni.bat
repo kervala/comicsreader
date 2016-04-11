1b061859c8aa0ba9a056ac305224df0076afb89f
Kervala <983521+kervala@users.noreply.github.com>
2016-04-11 14:25:20 +0200

    Fixed: Paths

--- app/generate_jni.bat
+++ app/generate_jni.bat
@@ -1,5 +1,5 @@
 set PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_60\bin
-cd build\intermediates\classes\comicsreader\debug
-javah -jni -classpath %SDK_ROOT%\platforms\android-23\android.jar;. -o ..\..\..\..\..\src\main\jni\unrar-jni\net_kervala_comicsreader_RarFile.h net.kervala.comicsreader.RarFile
+cd build\intermediates\classes\debug
+javah -jni -classpath %SDK_ROOT%\platforms\android-23\android.jar;. -o ..\..\..\..\src\main\jni\unrar-jni\net_kervala_comicsreader_RarFile.h net.kervala.comicsreader.RarFile
 
 pause
