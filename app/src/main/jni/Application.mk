a1496bec7c3cc0779b06f0f7893c6cc23865c6c7
Kervala <983521+kervala@users.noreply.github.com>
2017-10-21 16:11:32 +0200

    Changed: Use Android API 9 as minimum

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_ABI := armeabi-v7a armeabi x86 mips
-APP_PLATFORM := android-4
+APP_PLATFORM := android-9
 APP_STL := gnustl_static
 
 APP_CPPFLAGS += -fexceptions
